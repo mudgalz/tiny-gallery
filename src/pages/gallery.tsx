3321d301a4338391a62d8b927afed2ac9bbd6c82
Akash Mudgal <akashmudgal66403@gmail.com>
2024-09-11 01:03:23 +0530

    Revert "feat: added Image Viewer"

--- src/pages/gallery.tsx
+++ src/pages/gallery.tsx
@@ -5,16 +5,15 @@ import GallerySkeleton from "@/components/gallery/GallerySkeleton";
 import { AbsoluteCenteredWrapper } from "@/components/loader";
 import PaginationSelector from "@/components/ui/PaginationSelector";
 import useGalleryFilter from "@/hooks/useGalleryFilter";
-import { useGalleryStore } from "@/store/gallerySource";
 import { useQuery } from "@tanstack/react-query";
-import Viewer from "react-viewer";
+
 // Define the response types
 type GalleryResponse = PexelResponse | PixabayResponse;
 
 export default function Gallery() {
   const { currentPage, searchedQuery, finalQuery, handlePageChange, source } =
     useGalleryFilter();
-  const { selectedImageUrl, setSelectedImageUrl } = useGalleryStore();
+
   const pexelApiUrl = !!searchedQuery
     ? `search?${finalQuery}`
     : `curated?${finalQuery}`;
@@ -45,41 +44,29 @@ export default function Gallery() {
     flatData = data.hits;
     totalResults = data.totalHits;
   }
+
   return (
-    <>
-      <div className="w-full p-4 xl:px-0 flex-grow">
-        <GalleryHeader totalData={totalResults} loading={isLoading} />
-        <div className="columns-2 md:columns-3 lg:columns-4 gap-4 space-y-4 mt-4">
-          {isLoading ? (
-            <GallerySkeleton />
-          ) : (
-            flatData?.map((image) => (
-              <GalleryCard key={image.id} image={image} />
-            ))
-          )}
-        </div>
-        {!isLoading && !flatData?.length && (
-          <AbsoluteCenteredWrapper className="text-xl">
-            No Photos Found
-          </AbsoluteCenteredWrapper>
+    <div className="w-full p-4 xl:px-0 flex-grow">
+      <GalleryHeader totalData={totalResults} loading={isLoading} />
+      <div className="columns-2 md:columns-3 lg:columns-4 gap-4 space-y-4 mt-4">
+        {isLoading ? (
+          <GallerySkeleton />
+        ) : (
+          flatData?.map((image) => <GalleryCard key={image.id} image={image} />)
         )}
-        {!isLoading && totalResults !== undefined && flatData?.length ? (
-          <PaginationSelector
-            currentPage={currentPage}
-            onPageChange={(page) => handlePageChange(page)}
-            pages={Math.ceil(totalResults / 30)}
-          />
-        ) : null}
       </div>
-      <Viewer
-        images={[{ src: selectedImageUrl!, alt: "Image Preview" }]}
-        visible={!!selectedImageUrl}
-        onClose={() => setSelectedImageUrl("")}
-        noToolbar
-        noFooter
-        zoomSpeed={0.05}
-        className="z-20 relative"
-      />
-    </>
+      {!isLoading && !flatData?.length && (
+        <AbsoluteCenteredWrapper className="text-xl">
+          No Photos Found
+        </AbsoluteCenteredWrapper>
+      )}
+      {!isLoading && totalResults !== undefined && flatData?.length ? (
+        <PaginationSelector
+          currentPage={currentPage}
+          onPageChange={(page) => handlePageChange(page)}
+          pages={Math.ceil(totalResults / 30)}
+        />
+      ) : null}
+    </div>
   );
 }
